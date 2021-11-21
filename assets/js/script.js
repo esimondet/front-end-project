067f8fef0d533770468d3e1d8d2b65e0cb38ba03
Edison Simondet <edison.simondet@gmail.com>
2021-11-20 23:19:41 -0600

    srape scrap pivot random locaiton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,3 @@
-// require.config({
-//     paths: {
-//         'bluebird': './node_modules/bluebird/js/browser/bluebird',
-//         'cheerio': './node_modules/cheerio/lib/cheerio',
-//         'request-promise': './node_modules/request-promise/lib/rp'
-//     }
-// });
-
 //Marvel API Key
 var privKey = "f7b73285d9cec4962b01dc78e356e5b8a0e6b78f";
 var pubKey = "d2eac0264cc9a3719ac91f730963a3e8";
@@ -32,32 +24,9 @@ function getHeroData() {
             var hero = {
                 "bio": data.data.results[0].description,
                 "imageUrl": data.data.results[0].thumbnail.path + '.' + data.data.results[0].thumbnail.extension,
-                "placeOfOriginUrl": data.data.results[0].urls[1].url.split("?")[0].replace('(', '').replace(')', '').replace(/_/g, '-').replace('universe', 'characters').replace('http', 'https') + '/in-comics'
             }
 
             heroList.heros.push(hero);
-
-            // require(['bluebird', 'cheerio', 'request-promise'], function (bluebird, cheerio, request) {
-            //     request(hero.placeOfOriginUrl, (error, response, html) => {
-            //         if (!error && response.statusCode == 200) {
-            //             alert(html);
-            //             const $ = cheerio.load(html);
-            //             var parent = $(".railBioInfoItem__label:contains('Place of Origin')").parent;
-            //             var origin = parent.find(".railBioLinks").val();
-            //             alert(origin);
-            //         }
-            //     });
-            // });
-
-            fetch(hero.placeOfOriginUrl, {
-                mode: 'no-cors',
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
-                    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token'
-                }
-            }).then(data => { console.log(data); });
-
             console.log(heroList);
         })
         .fail(function (err) {
@@ -74,10 +43,5 @@ var heroList = {
     "heros": []
 }
 
-
-
 //for hero mispelled: "Looks like that hero is unavailable! Try another"
 //for hero bio &/or image blank: if (value = null || "") { display  "CLASSIFIED"}
-
-//https://gateway.marvel.com:443/v1/public/characters?name=hulk&apikey=d2eac0264cc9a3719ac91f730963a3e8
-//https://gateway.marvel.com:433/v1/public/characters?name=hulk&ts=1637102859893&apikey=d2eac0264cc9a3719ac91f730963a3e8&hash=83ea153556699b9a1027a3e058859bb2
