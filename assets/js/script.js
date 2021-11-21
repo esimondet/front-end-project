ee474dbe6bc32711be2d6464a6010d40dd80e539
Edison Simondet <edison.simondet@gmail.com>
2021-11-20 19:17:38 -0600

    Return Bio and Image from API into JSON array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,62 @@
+// const request= require("request-promise");
+// const cheerio= require("cheerio");
+
 //Marvel API Key
 var privKey = "f7b73285d9cec4962b01dc78e356e5b8a0e6b78f";
 var pubKey = "d2eac0264cc9a3719ac91f730963a3e8";
 
-function getHeroId() {
+function getHeroData() {
 
     //per API documentation, new timestamp needed with every request
     var ts = new Date().getTime();
     //per terms of use, privKey must be encrypted 
     var hash = CryptoJS.MD5(ts + privKey + pubKey).toString();
 
-    var heroName = "hulk"; //$("#hero");
-    
+    var heroName = $("#heroName").val();
+
     var url = "https://gateway.marvel.com/v1/public/characters?name=" + heroName;
 
-    console.log(url);
     $.getJSON(url, {
         ts: ts,
         apikey: pubKey,
         hash: hash,
 
     })
-    .done(function(data) {
-        console.log(data);
-        //return Id
-    })
-    .fail(function(err) {
-        console.log(err); //error codes found on developer.marvel.com
-    });
+        .done(function (data) {
+
+            var hero = {
+                "bio": data.data.results[0].description,
+                "imageUrl": data.data.results[0].thumbnail.path + '.' + data.data.results[0].thumbnail.extension,
+                "placeOfOriginUrl": data.data.results[0].urls[1].url.split("?")[0].replace('(', '').replace(')', '').replace(/_/g, '-').replace('universe', 'characters') + '/in-comics'
+            }
+
+            heroList.heros.push(hero);
+
+            // request(hero.placeOfOriginUrl, error, response, html) => {
+            //     if (!error && response.statusCode == 200) {
+            //         const $= cherio.load(html);
+
+            //     }
+            // }
+            console.log(heroList);
+        })
+        .fail(function (err) {
+            console.log(err); //error codes found on developer.marvel.com
+        });
 };
 
-getHeroId(); //$(document).on("click", "#heroBtn", getHeroId());
+$(document).on('click', '#heroBtn', function (event) {
+    event.preventDefault();
+    getHeroData();
+})
+
+var heroList = {
+    "heros": []
+}
+
+
+//for hero mispelled: "Looks like that hero is unavailable! Try another"
+//for hero bio &/or image blank: if (value = null || "") { display  "CLASSIFIED"}
 
 //next step scrape
 
