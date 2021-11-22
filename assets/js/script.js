84c0ab0341ab0ee590902480e6e76d0748cc5bb1
Edison Simondet <edison.simondet@gmail.com>
2021-11-22 00:22:25 -0600

    display API results on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,17 +22,54 @@ function getHeroData() {
         .done(function (data) {
 
             var hero = {
-                //"name": , tomorrow, add hero name to the array
+                "name": data.data.results[0].name,
                 "bio": data.data.results[0].description,
                 "imageUrl": data.data.results[0].thumbnail.path + '.' + data.data.results[0].thumbnail.extension,
             }
 
             heroList.heros.push(hero);
             console.log(heroList);
+
+            var heroDiv = $("<div class='card'>");
+
+            //hero profile picture
+            var cardContentDiv = $("<div class='card-content'>");
+            var mediaDiv = $("<div class='media'>");
+            var mediaLeftDiv = $("<div class='media-left'>");
+            var heroFigure = $("<figure class='image is-48x48'>");
+            var heroImg = $("<img src='" + hero.imageUrl + "' alt='" + hero.name + " thumbnail'>");
+
+            heroFigure.append(heroImg);
+            mediaLeftDiv.append(heroFigure);
+            mediaDiv.append(mediaLeftDiv);
+            
+
+            //hero name
+            var mediaTitleDiv = $("<div class='media-content'>");
+            var titleP = $("<p class='title is-4'>");
+            titleP.text(hero.name);
+
+            mediaTitleDiv.append(titleP);
+            mediaDiv.append(mediaTitleDiv);
+            cardContentDiv.append(mediaDiv);
+
+            //hero bio
+            var contentClass = $("<div class='content'>");
+            cardContentDiv.append(contentClass);
+            contentClass.html(hero.bio);
+
+            //final assembly
+
+            heroDiv.append(cardContentDiv);
+
+            $("#heroGen").append(heroDiv);
+
         })
         .fail(function (err) {
             console.log(err); //error codes found on developer.marvel.com
         });
+
+
 };
 
 $(document).on('click', '#heroBtn', function (event) {
